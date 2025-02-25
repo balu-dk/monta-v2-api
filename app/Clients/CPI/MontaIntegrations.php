8608866bc93da9e1991ea66501c8441c243bd515
nordiccharge <sebastian@nordiccharge.com>
2025-02-25 09:06:35 +0100

    Added Chargepoint integration link

--- app/Clients/CPI/MontaIntegrations.php
+++ app/Clients/CPI/MontaIntegrations.php
@@ -23,7 +23,7 @@ class MontaIntegrations {
             return [
                 'status' => '200',
                 'message' => 'Successfully got integration link',
-                'link' => $response->headers()['location']
+                'link' => $response->headers()['location'][0]
             ];
         }
 
